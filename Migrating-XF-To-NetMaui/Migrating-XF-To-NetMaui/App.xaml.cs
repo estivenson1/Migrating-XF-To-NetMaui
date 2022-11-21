e82a779960634c4d25d37177d1f6e8e79edcaca4
Estivenson Ortega Villar <eortegav@intergrupo.com>
2022-11-21 15:41:29 -0500

    Creación del proyecto Migrating-XF-To-NetMaui

---
+++ Migrating-XF-To-NetMaui/Migrating-XF-To-NetMaui/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Migrating_XF_To_NetMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
